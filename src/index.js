aa316c4a55f12a699d9e9b4f1b9ec702a767b77f
srichmond3000 <srichmond3000@googlemail.compwd>
2017-06-10 12:00:51 +0100

    Work on index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import App from './containers/App';
+import store from '../store/store';
+import { setTopic, setDisplayMode } from '../actions/actions';
+
+store.dispatch(setTopic('javascript'));
+store.dispatch(setDisplayMode('THUMBNAIL'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
+
+function showState() {
+  const state = store.getState();
+  /* eslint-disable */
+  debugger
+  /* eslint-enable */
+}
+
+store.subscribe(showState);
