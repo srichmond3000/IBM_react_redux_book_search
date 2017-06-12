0ac66e9a6bddc5adf5800c67c77cc315dcb636cc
srichmond3000 <srichmond3000@googlemail.compwd>
2017-06-12 06:22:52 +0100

    Created the App and Title components.

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const titleStyle = {
+  fontFamily: 'tahoma',
+  fontSize: '24px',
+  textAlign: 'center'
+};
+
+const Title = () => (
+  <div style={titleStyle}>
+    Book Search
+  </div>
+);
+
+export default Title;
---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ControlsContainer from './ControlsContainer';
+import Title from '../components/Title';
+
+const App = () => (
+  <div>
+    <Title />
+    <hr />
+    <ControlsContainer />
+  </div>
+);
+
+export default App;
